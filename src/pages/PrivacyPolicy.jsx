6762f22fa09271a7ca79b7ba22ab2cf7817869af
Hiba Inamdar <148106496+Hibainam@users.noreply.github.com>
2025-04-16 17:29:57 +0530

    update Privacy Policy page

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -7,11 +7,11 @@ function PrivacyPolicy() {
     },[]);
     return (
         <div className="max-w-screen-3xl mx-auto bg-light-blue-50">
-            <div className="flex justify-center items-center bg-light-blue-200 h-auto">
-                <div>
-                    <p className="text-4xl md:text-6xl lg:text-6xl m-6 lg:m-32 text-[#1E2761]">Privacy Policy</p>
-                </div>
+       <div className="flex justify-center items-center py-4">
+            <div>
+            <p className="text-4xl md:text-5xl lg:text-6xl font-bold m-6 lg:m-20 text-[#161e4a]">Privacy Policy</p>
             </div>
+        </div>    
             <div className="mx-4 my-6 md:ml-20 md:mr-12 lg:ml-36 lg:mr-24">
                 <p className="text-3xl font-bold">Our Commitment to You</p>
                 <p className="mt-6 text-lg">At CarTechIndia, your privacy is a top priority. Your privacy is at the core of the way we design and build the services and products you know and love, so that you can fully trust them and focus on building meaningful connections.</p>
@@ -35,15 +35,15 @@ function PrivacyPolicy() {
                     9. Privacy Policy Changes <br />
                     10. How To Contact Us</p>
 
-                <p className="text-2xl font-bold mt-8">1. Who We Are</p>
+                <p className="text-xl font-bold mt-8">1. Who We Are</p>
                 <p className="mt-6 text-lg">The company that is responsible for your information under this Privacy Policy (the “data controller”) is:</p>
                 {/* <p className="mt-4 text-lg">CarTechIndia Pvt. Ltd. <br/> 1128, 3rd floor, Rd Number 36, <br/> Jubilee Square, Jubilee Hills, <br/> Pune, Maharashtra 500033.</p> */}
 
-                <p className="text-2xl font-bold mt-8">2. Where This Privacy Policy Applies</p>
+                <p className="text-xl font-bold mt-8">2. Where This Privacy Policy Applies</p>
 
                 <p className="mt-6 text-lg">This Privacy Policy applies to websites, apps, events and other services operated by CarTechIndia. For simplicity, we refer to all of these as our “services” in this Privacy Policy.</p>
 
-                <p className="text-2xl font-bold mt-8">3. Information We Collect</p>
+                <p className="text-xl font-bold mt-8">3. Information We Collect</p>
 
                 <p className="mt-6 text-lg">We collect basic profile details and the training you use our services, for example access logs, as well as information from third parties, like when you access our services through a social media account. If you want additional info, we go into more detail below.</p>
                 <p className="mt-4 ml-8 text-lg">When you participate in surveys or focus groups, you give us your insights into our products and services, responses to our questions and testimonials. <br />
@@ -54,13 +54,13 @@ function PrivacyPolicy() {
                     When you take tests, we collect your performance <br />
                     Of course, we also process your chats with other users as well as the content you publish, as part of the operation of the services.</p>
 
-                <p className="mt-6 text-2xl">Information we receive from others</p>
+                <p className="mt-6 text-xl">Information we receive from others</p>
                 <p className="mt-4 text-lg">In addition to the information you provide us directly, we receive information about you from others, including:</p>
                 <p className="mt-4 ml-8 text-lg"><b>Other Users </b> Other users may provide information about you as they use our services. For instance, we may collect information about you from other users if they contact us about you. <br />
                     <b>Social Media</b> You may be able to use your social media login (such as Facebook Login) to create and log into your CarTechIndia account. This saves you from having to remember yet another user name and password and allows you to share some information from your social media account with us. <br />
                     <b>Other Partners</b> We may receive info about you from other social networking platforms or from our partners, for instance where CarTechIndia ads are published on a partner’s websites and platforms (in which case they may pass along details on a campaign’s success).</p>
 
-                <p className="mt-6 text-2xl">Information collected when you use our services</p>
+                <p className="mt-6 text-xl">Information collected when you use our services</p>
                 <p className="mt-4 text-lg">When you use our services, we collect information about which features you’ve used, how you’ve used them and the devices you use to access our services. See below for more details:</p>
                 <p className="mt-4 text-lg"><b>Usage Information</b> We collect information about your activity on our services, for instance how you use them (e.g., date and time you logged in, features you’ve been using, searches, clicks and pages which have been shown to you, referring web page address, advertising that you click on) and how you interact with other users (e.g., users you connect and interact with, time and date of your exchanges, number of messages you send and receive).</p>
                 <p className="mt-4 text-lg"><b>Device information</b> We collect information from and about the device(s) you use to access our services, including:</p>
@@ -68,11 +68,11 @@ function PrivacyPolicy() {
                     information on your wireless and mobile network connection, like your service provider and signal strength; <br />
                     information on device sensors such as accelerometers, gyroscopes and compasses.</p>
 
-                <p className="text-2xl font-bold mt-8">4. Cookies and Other Similar Data Collection Technologies</p>
+                <p className="text-xl font-bold mt-8">4. Cookies and Other Similar Data Collection Technologies</p>
                 <p className="mt-6 text-lg">We use and may allow others to use cookies and similar technologies (e.g., web beacons, pixels) to recognize you and/or your device(s). We use cookie related information (such as authenticating you, remembering your preferences and settings, analyzing site traffic and trends, delivering and measuring the effectiveness of advertising campaigns, allowing you to use social features) and how you can better control their use, through your browser settings and other tools.</p>
                 <p className="mt-4 text-lg">Some web browsers (including Safari, Internet Explorer, Firefox and Chrome) have a “Do Not Track” (“DNT”) feature that tells a website that a user does not want to have his or her online activity tracked. If a website that responds to a DNT signal receives a DNT signal, the browser can block that website from collecting certain information about the browser’s user. Not all browsers offer a DNT option and DNT signals are not yet uniform. For this reason, many businesses, including CarTechIndia, do not currently respond to DNT signals.</p>
 
-                <p className="text-2xl font-bold mt-8">5. How We Use Information</p>
+                <p className="text-xl font-bold mt-8">5. How We Use Information</p>
                 <p className="mt-6 text-lg">The main reason we use your information is to deliver and improve our services. Additionally, we use your info to help keep you safe and to provide you with advertising that may be of interest to you. Read on for a more detailed explanation of the various reasons we use your information, together with practical examples.</p>
                 <p className="mt-4 text-xl"><b>To improve our services and develop new ones, we</b></p>
                 <p className="mt-4 ml-8 text-lg">Administer focus groups and surveys <br />
@@ -91,23 +91,19 @@ function PrivacyPolicy() {
                     Legitimate interests: We may use your information where we have legitimate interests to do so. For instance, we analyze users’ behavior on our services to continuously improve our offerings, we suggest offers we think might interest you, and we process information for administrative, fraud detection and other legal purposes. <br />
                     Consent: From time to time, we may ask for your consent to use your information for certain specific reasons. You may withdraw your consent at any time by contacting us at the address provided at the end of this Privacy Policy.</p>
 
-                <p className="text-2xl font-bold mt-8">6. How We Share Information</p>
+                <p className="text-xl font-bold mt-8">6. How We Share Information</p>
                 <p className="mt-6 text-lg">Since our goal is to help you make meaningful connections, the main sharing of users’ information is, of course, with other users. We also share some users’ information with service providers and partners who assist us in operating the services, with other Match Group companies and, in some cases, legal authorities. Read on for more details about how your information is shared with others.</p>
 
                 <p className="mt-4"><b>With our service providers and partners</b></p>
                 <p className="mt-2 text-lg">We use third parties to help us operate and improve our services. These third parties assist us with various tasks, including data hosting and maintenance, analytics, customer care, marketing, advertising, payment processing and security operations.</p>
                 <p className="mt-2 text-lg">We may also share information with partners who distribute and assist us in advertising our services. For instance, we may share limited information on you in hashed, non-human readable form to advertising partners.</p>
                 <p className="mt-4 text-lg">We follow a strict vetting process prior to engaging any service provider or working with any partner. All of our service providers and partners must agree to strict confidentiality obligations.</p>
-
                 <p className="mt-4 text-xl"><b>For corporate transactions</b></p>
                 <p className="mt-2 text-lg">We may transfer your information if we are involved, whether in whole or in part, in a merger, sale, acquisition, divestiture, restructuring, reorganization, dissolution, bankruptcy or other change of ownership or control.</p>
-
                 <p className="mt-4 text-xl"><b>When required by law</b></p>
                 <p className="mt-2 text-lg">We may disclose your information if reasonably necessary: (i) to comply with a legal process, such as a court order, subpoena or search warrant, government / law enforcement investigation or other legal requirements; (ii) to assist in the prevention or detection of crime (subject in each case to applicable law); or (iii) to protect the safety of any person.</p>
-
                 <p className="mt-4 text-xl"><b>To enforce legal rights</b></p>
                 <p className="mt-2 text-lg">We may also share information: (i) if disclosure would mitigate our liability in an actual or threatened lawsuit; (ii) as necessary to protect our legal rights and legal rights of our users, business partners or other interested parties; (iii) to enforce our agreements with you; and (iv) to investigate, prevent, or take other action regarding illegal activity, suspected fraud or other wrongdoing.</p>
-
                 <p className="mt-4 text-xl"><b>With your consent or at your request</b></p>
                 <p className="mt-2 text-lg">We may ask for your consent to share your information with third parties. In any such case, we will make it clear why we want to share the information.</p>
                 <p className="mt-4 text-lg">We may use and share non-personal information (meaning information that, by itself, does not identify who you are such as device information, general demographics, general behavioral data, geolocation in de-identified form), as well as personal information in hashed, non-human readable form, under any of the above circumstances.</p>
@@ -118,15 +114,11 @@ function PrivacyPolicy() {
                 <p className="mt-4 text-lg">Also, we may not be able to accommodate certain requests to object to the processing of personal information, notably where such requests would not allow us to provide our service to you anymore. For instance, we cannot provide our service if we do not have your date of birth.</p>
                 <p className="mt-4 ml-8 text-lg"><u>Uninstall.</u> You can stop all information collection by an app by uninstalling it using the standard uninstall process for your device. If you uninstall the app from your mobile device, the unique identifier associated with your device will continue to be stored. If you reinstall the application on the same mobile device, we will be able to re-associate this identifier to your previous transactions and activities. <br />
                     <u>Accountability.</u> In certain countries, including in the European Union, you have a right to lodge a complaint with the appropriate data protection authority if you have concerns about how we process your personal information. The data protection authority you can lodge a complaint with notably may be that of your habitual residence, where you work or where we are established.</p>
-
-                <p className="text-2xl font-bold mt-8">7. How We Protect Your Information</p>
-
+                <p className="text-xl font-bold mt-8">7. How We Protect Your Information</p>
                 <p className="mt-4 text-lg">We work hard to protect you from unauthorized access to or alteration, disclosure or destruction of your personal information. As with all technology companies, although we take steps to secure your information, we do not promise, and you should not expect, that your personal information will always remain secure.</p>
                 <p className="mt-4 text-lg">We regularly monitor our systems for possible vulnerabilities and attacks and regularly review our information collection, storage and processing practices to update our physical, technical and organizational security measures.</p>
                 <p className="mt-4 text-lg">We may suspend your use of all or part of the services without notice if we suspect or detect any breach of security. If you believe that your account or information is no longer secure, please notify us immediately on <u>asif.attar@caryanam.in</u></p>
-
-                <p className="text-2xl font-bold mt-8">8. How Long We Retain Your Information</p>
-
+                <p className="text-xl font-bold mt-8">8. How Long We Retain Your Information</p>
                 <p className="mt-4 text-lg">We keep your personal information only as long as we need it for legitimate business and as permitted by applicable law. To protect the safety and security of our users on and off our services, we implement a safety retention window of three months following account deletion. During this period, account information will be retained although the account will of course not be visible on the services anymore.</p>
                 <p className="mt-4 text-lg">In practice, we delete or anonymize your information upon deletion of your account (following the safety retention window) or after two years of continuous inactivity, unless:</p>
                 <p className="mt-4 ml-8 text-lg">1. We must keep it to comply with applicable law (for instance, some “traffic data” is kept for one year to comply with statutory data retention obligations); <br />
@@ -134,11 +126,9 @@ function PrivacyPolicy() {
                     3. There is an outstanding issue, claim or dispute requiring us to keep the relevant information until it is resolved; or <br />
                     4. The information must be kept for our legitimate business interests, such as fraud prevention and enhancing users safety and security. For example, information may need to be kept to prevent a user who was banned for unsafe behavior or security incidents from opening a new account.</p>
                 <p className="mt-4 text-lg">Keep in mind that even though our systems are designed to carry out data deletion processes according to the above guidelines, we cannot promise that all data will be deleted within a specific time frame due to technical constraints.</p>
-
-                <p className="text-2xl font-bold mt-8">9. Privacy Policy Changes</p>
+                <p className="text-xl font-bold mt-8">9. Privacy Policy Changes</p>
                 <p className="mt-4 text-lg">Because we’re always looking for new and innovative ways to help you build meaningful connections, this policy may change over time. We will notify you before any material changes take effect so that you have time to review the changes.</p>
-
-                <p className="text-2xl font-bold mt-8">10. How to Contact Us</p>
+                <p className="text-xl font-bold mt-8">10. How to Contact Us</p>
                 <p className="mt-4 text-lg">If you have questions about this Privacy Policy, here’s how you can reach us:</p>
                 <p className="mt-4 text-lg">Online: <u>asif.attar@caryanam.in</u></p>
                 <p className="mt-4 text-lg">By phone: 7755994123</p>
@@ -147,5 +137,4 @@ function PrivacyPolicy() {
         </div>
     )
 }
-
 export default PrivacyPolicy
